b7d1ebe07783ba8594c150afcd36f0ff95d9d0ae
deluca42 <deluca42@gmail.com>
2024-03-13 18:12:10 -0300

    concatallfiles .csv

--- concatAllExcelFiles.py
+++ concatAllExcelFiles.py
@@ -1,1 +1,17 @@
-aa
+import pandas as pd 
+import os 
+
+def read_transform(path):
+    df = pd.read_csv(path)
+    colums = df.columns[:2]
+    df = df[colums].dropna(how='any')
+    df = df.set_index(["ano","uf"])
+    return df
+
+file_path = "deluca-ubuntu/all_files/"
+filenames = os.listdir(file_path)
+
+dfs = [read_transform(file_path+i) for i in filenames]
+
+df_full =pd.concat(dfs, axis=l)
+df_full.to_csv("deluca-ubuntu/all_files/consolidado.csv")
